f48f53fc5656f4c0e2d1dcec9ccc6324d04aa42e
AntonioNava-An70 <antonio.nava@unosquare.com>
2022-08-03 23:29:28 -0400

    Added PNG Image of HTML report

--- tests/google/firstGoogle.test.js
+++ tests/google/firstGoogle.test.js
@@ -1,8 +1,22 @@
 describe('Demo Test for grouping', () => {
+	it('Title should be Google', () => {
+		browser
+			.url('https://www.google.com')
+			.maximizeWindow()
+			.assert.titleEquals('Google')
+	})
+
 	it('Should be Open Google home page', () => {
 		browser
 			.url('https://www.google.com')
 			.maximizeWindow()
-			.assert.urlEquals('https://www.google.com/')
+			.assert.urlEquals('https://www.google.com')
+	})
+
+	it('Verify that image Google is displayed', () => {
+		browser
+			.url('https://www.google.com')
+			.maximizeWindow()
+			.verify.visible('img[alt="Google"]')
 	})
 })
